c17922fbe5eb601dfbf9bb70c954a72f874f0998
Ellinordmark <109726891+Ellinordmark@users.noreply.github.com>
2023-12-01 15:41:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ console.log(output);
 //Test om hämtningen funkar
 console.log(textfields);
 
-//Loopar igenom textfälten
+//Loopar igenom textfälten och kollar om man lämnar de (blur)
 textfields.forEach((field) => field.addEventListener("blur", handleBlur));
 
-//Eventlistener för button
+//Eventlistener för button på click
 button.addEventListener("click", handleClick);
 
+// Handle click
 function handleClick(e) {
   console.log("handleclick", e);
   //   console.log(e);
 }
 
+// Lägger till vad man skriver i fälten Färg och Innehåll i den tomma diven (output) när man lämnar textfälten
 function handleBlur(e) {
   const name = e.target.name;
   const value = e.target.value;
   const html = `<p>Namn ${name}, Value ${value}</p>`;
   output.insertAdjacentHTML("afterend", html);
-
-  //   console.log(e);
 }

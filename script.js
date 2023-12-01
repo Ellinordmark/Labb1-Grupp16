9d2998a2498da57d4ec2efc954224788e5aad6af
Ellinordmark <109726891+Ellinordmark@users.noreply.github.com>
2023-12-01 15:40:06 +0100

    Lägger till html i diven

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const checkbox = document.getElementById("divStyle");
 const button = document.getElementById("remove");
 const textfields = document.querySelectorAll(".textfield");
-const box = document.getElementById("box");
+const output = document.getElementById("output");
+console.log(output);
 
 //Test om hämtningen funkar
 console.log(textfields);
 
 //Loopar igenom textfälten
-textfields.forEach((field) => field.addEventListener("click", handleClick));
-
-function handleClick(e) {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   output = "hej";
-  console.log(e);
-}
+textfields.forEach((field) => field.addEventListener("blur", handleBlur));
 
 //Eventlistener för button
 button.addEventListener("click", handleClick);
+
+function handleClick(e) {
+  console.log("handleclick", e);
+  //   console.log(e);
+}
+
+function handleBlur(e) {
+  const name = e.target.name;
+  const value = e.target.value;
+  const html = `<p>Namn ${name}, Value ${value}</p>`;
+  output.insertAdjacentHTML("afterend", html);
+
+  //   console.log(e);
+}
